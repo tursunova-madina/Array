9802c6b4c31a383c745faab261b8d39e07aede4e
tursunova-madina <tmedina0804@gmail.com>
2022-08-24 21:47:19 +0500

    array

---
+++ app.js
@@ -0,0 +1,27 @@
+"use strict";
+// Steven wants to build a very simple tip calculator for whenever he goes eating
+// in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50
+// and 300. If the value is different, the tip is 20%.
+
+// const billValue = +prompt("Total bill:");
+// const tipValueFirst = 15 / 100;
+// const tipValueSecond = 20 / 100;
+
+// billValue >= 50 && billValue <= 300
+//   ? console.log(
+//       "You should pay " + (billValue * tipValueFirst + billValue) + " $"
+//     )
+//   : console.log(
+//       "You should pay " + (billValue * tipValueSecond + billValue) + " $"
+//     );
+
+// n-2
+// Arraydagi sonlarni bir biriga qo'shib natijani qaytarib beruvchi funksiya tuzing.
+
+// let numbers = [1, 3, 5, 9];
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+
+// console.log(sum);
